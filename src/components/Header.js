5cd11b41546130a84c93ad24d5bcaf68e5a08e18
juanmoyano123 <moyanojjeronimo@gmail.com>
2022-10-04 12:21:24 -0300

    update forms y cart dinamico

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,19 @@ import '../styles//Header.css'
 import {Link as LinkRouter} from 'react-router-dom'
 import Login from './Login'
 import ShoppingCart from './ShoppingCart'
+import {  useState } from "react";
+
 
 export default function Header() {
+    const [open, setOpen] = useState(false);
+
+    const openMenu = () => {
+        if (open === true) {
+          setOpen(false);
+        } else {
+          setOpen(true);
+        }
+      };  
     return (
         <>
             <div className="navBar navbar bg-black">
@@ -27,39 +38,18 @@ export default function Header() {
                 </div>
                 <div className="navbar-end">
                     
-                    <div className="dropdown dropdown-end">
-                        <label tabIndex={0} className="btn btn-ghost btn-circle">
-                            <div className="indicator">
+                    <div className="dropdown dropdown-end" onClick={openMenu}>
+                        <label  tabIndex={0} className="btn btn-ghost btn-circle">
+                            <div className="indicator" >
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
                                 <span className="badge badge-sm indicator-item">8</span>
                             </div>
                         </label>
-<ShoppingCart/>
-                          {/*   <div className="card-body">
-                                <span className="font-bold text-lg">8 Items</span>
-                                <span className="text-info">Subtotal: $999</span>
-                                <div className="card-actions">
-                                    <button className="bG-color btn btn-block">View cart</button>
-                                </div>
-                            </div> */}
+                        {open? (<ShoppingCart/>): null}
+
                     </div>
                     <div className="dropdown dropdown-end">
-
                         <Login/>
-                    {/*     <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                            <div className="w-10 rounded-full">
-                                <img src="https://placeimg.com/80/80/people" />
-                            </div>
-                        </label>
-                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-black rounded-box w-52">
-                            <li>
-                                <a className="justify-between">
-                                    Profile
-                                </a>
-                            </li>
-                            <li><a>Settings</a></li>
-                            <li><a>Logout</a></li>
-                        </ul>  */}
                     </div>
 
                 </div>
