1c40c5ed3d17cd5c8d35872157f0fd7fd3f0aadb
ulisescolosimo <colosimo101@gmail.com>
2022-10-05 18:20:58 -0300

    Fix header modal

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {Link as LinkRouter, useNavigate} from 'react-router-dom'
 
 const ShoppingCart = () => {
 
@@ -11,7 +11,7 @@ const ShoppingCart = () => {
                         <span className="font-bold text-lg">8 Items</span>
                         <span className="text-info">Subtotal: $999</span>
                         <div className="card-actions">
-                            <button className="bG-color btn btn-block">View cart</button>
+                            <button className="bG-color btn btn-block"><LinkRouter to="/cart">View cart</LinkRouter></button>
                         </div>
                     </div>
                   
