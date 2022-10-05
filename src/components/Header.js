e99869bc6c2263d380e0857db616d89452d4160e
facundolaso <lasofacundo@gmail.com>
2022-10-05 17:21:48 -0300

    agregado de about us en header

--- src/components/Header.js
+++ src/components/Header.js
@@ -62,11 +62,9 @@ export default function Header() {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-black rounded-box w-52">
-                            <li><a>Home</a></li>
+                            <li><LinkRouter to="/home">Home</LinkRouter></li>
                             <li><LinkRouter to="/products">Products</LinkRouter></li>
-                            <li><a>¿Quienes Somos?</a></li>
-                            <li><a>Locales y ¿Donde puedes encontrarnos?</a></li>
-                            <li><a>Contactanos! </a></li> 
+                            <li><LinkRouter to="/aboutUs">About Us</LinkRouter></li>
                         </ul>
                     </div>
                 </div>
