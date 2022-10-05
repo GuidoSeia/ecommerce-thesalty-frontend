2f12d47dc5356e3286bc118e6847f8cb0b627e45
juanmoyano123 <moyanojjeronimo@gmail.com>
2022-10-05 18:56:20 -0300

    estilos

--- src/pages/AdminProfile.js
+++ src/pages/AdminProfile.js
@@ -3,6 +3,8 @@ import PageLayout from '../components/layout/PageLayout'
 import NewProducts from "./NewProducts"
 import EditProducts from "./EditProducts"
 import {Link as LinkRouter} from 'react-router-dom'
+import '../styles//AdminProfile.css'
+
 
 export default function AdminProfile() {
 
@@ -10,30 +12,30 @@ export default function AdminProfile() {
   return (
     <div>
 <PageLayout>
+<div className="bodyContainer">
 
-<div className="card w-96 bg-base-100 shadow-xl">
-  <figure><img src="" alt="Shoes" /></figure>
-  <div className="card-body">
+<div className="cardContainer">
+  <figure><img src="/billeteras.webp" alt="Shoes" /></figure>
+  <div className="subContainer">
     <h2 className="card-title">Edit Products!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary"><LinkRouter className="btn btn-primary btn-home-page text-xs" to={'/products'}>Edit Now!</LinkRouter></button>
+    <div className="">
+      <button className=""><LinkRouter className="btn btn-primary btn-home-page text-xs" to={'/products'}>Edit Now!</LinkRouter></button>
     </div>
   </div>
 </div>
 
-<div className="card w-96 bg-base-100 shadow-xl">
-  <figure><img src="" alt="Shoes" /></figure>
-  <div className="card-body">
+<div className="cardContainer">
+  <figure><img src="/relojes-hombre-abaco.webp" alt="Shoes" /></figure>
+  <div className="subContainer">
     <h2 className="card-title">Create a Product!</h2>
-    <p></p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary"><LinkRouter className="btn btn-primary btn-home-page text-xs" to={'/newproduct'}>Create Now!</LinkRouter></button>
+    
+    <div className="">
+      <button className=""><LinkRouter className="btn btn-primary btn-home-page text-xs" to={'/newproduct'}>Create Now!</LinkRouter></button>
     </div>
   </div>
 </div>
 
-
+</div>
 
 {/* 
    <EditProducts/>
