68c18db571838691a5b40e1a04705f833e7c05c6
LauraMartinez20 <danielamartinez.1012@gmail.com>
2022-10-05 21:56:16 -0500

    create page Details

---
+++ src/pages/Details.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import DetailCards from '../components/DetailCards';
+import PageLayout from '../components/layout/PageLayout';
+
+
+  const Details = () => {
+    return (
+      <PageLayout>
+      <DetailCards/>
+      </PageLayout>
+    )
+  }
+  
+  export default Details
