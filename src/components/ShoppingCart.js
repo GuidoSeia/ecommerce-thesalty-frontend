0ae17f2099956241fb7bdc261248fa3a1291b38b
ulisescolosimo <colosimo101@gmail.com>
2022-10-06 11:12:38 -0300

    Fix total price

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import {Link as LinkRouter, useNavigate} from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux';
+import { current } from 'daisyui/src/colors';
 
 const ShoppingCart = () => {
 
+  const cart = useSelector((state) => state.cart.cart)
+
+  console.log(cart);
+
+  const addition = (acc, currentValue) => {
+    return acc+currentValue.price*currentValue.quantity
+  }
+  
+  let total = cart.reduce(addition, 0);
 
   return (
-<div tabIndex={0} className="mt-3 card card-compact dropdown-content w-52 bg-black shadow" onClick={onclick} >
+<div tabIndex={0} className="mt-3 card flex justify-center items-center card-compact dropdown-content w-52 bg-black shadow" onClick={onclick} >
 
-<div className="card-body"  >
-                        <span className="font-bold text-lg">8 Items</span>
-                        <span className="text-info">Subtotal: $999</span>
+<div className="card-body" >
+                        <span className="font-bold text-xl">{cart.length} items</span>
+                        <span className="text-info text-xl">$ {total}</span>
                         <div className="card-actions">
                             <LinkRouter className="bG-color btn btn-block" to="/cart">View cart</LinkRouter>
                         </div>
