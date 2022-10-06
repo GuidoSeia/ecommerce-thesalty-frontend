0e0ad65bc0f81f315b8a13d8b2c0942e8dac92da
ulisescolosimo <colosimo101@gmail.com>
2022-10-06 18:27:25 -0300

    Agregamos slice de cupon al store

--- src/features/store.js
+++ src/features/store.js
@@ -3,13 +3,15 @@ import productsApi from './productsApi'
 import usersAPI from './usersAPI'
 import loggedSlice from './loggedSlice'
 import cartSlice from './cartSlice'
+import codeSlice from './codeSlice'
 
 export default  configureStore({
     reducer: {
         [productsApi.reducerPath]: productsApi.reducer,
         [usersAPI.reducerPath]: usersAPI.reducer,
         logged: loggedSlice,
-        cart: cartSlice
+        cart: cartSlice,
+        code: codeSlice,
     },
     middleware: (getAllCities) => getAllCities({
         immutableCheck: false,
