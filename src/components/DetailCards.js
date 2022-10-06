f7501360cf7a315a32d690b4753f729dd4969ce4
ulisescolosimo <colosimo101@gmail.com>
2022-10-06 11:40:47 -0300

    Fix details cards

--- src/components/DetailCards.js
+++ src/components/DetailCards.js
@@ -5,15 +5,21 @@ import apiurl from '../api';
 import axios from 'axios'
 import { useDispatch, useSelector } from 'react-redux';
 import { addToCart } from '../features/cartSlice'
+import {useNavigate} from 'react-router-dom'
 
 export default function DetailCards() {
 
   let params = window.location.search
-  let urlParams = new URLSearchParams(params)
-  let productId = urlParams.get("productId")
-  const dispatch = useDispatch()
-  const cart = useSelector((state) => state.cart.cart)
 
+  let urlParams = new URLSearchParams(params)
+
+  let productId = urlParams.get("productId")
+  
+  const navigate = useNavigate()
+
+  const dispatch = useDispatch()
+
+  const cart = useSelector((state) => state.cart.cart)
 
   const [detailCards, setDetailCards] = useState([])
 
@@ -56,17 +62,19 @@ export default function DetailCards() {
           />
         </div>
       </div>
-      <div className="mx-auto px-4 pt-10 pb-16 ">
-        <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+      <div className="mx-auto px-4 pt-10 pb-16 flex justify-center items-center flex-wrap">
+        <div className="lg:col-span-2 lg:pr-8 mx-5">
           <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-2xl">{detailCards.brand}</h1>
           <div className="mt-4 lg:row-span-3 lg:mt-0">
-            <p className="text-base sm:text-2xl tracking-tight py-3 text-gray-900">{detailCards.description}</p>
+            <p className="text-2xl tracking-tight py-3 text-gray-900">{detailCards.description}</p>
           </div>
         </div>
-        <div className="py-2">
-        <p className="text-base md:text-2xl tracking-tight py-2 text-gray-900">Model: {detailCards.model}</p>
-          <p className="text-base md:text-2xl tracking-tight py-2 text-gray-900">Price: {detailCards.price}</p>
-          <p className="text-base md:text-2xl tracking-tight py-2 text-gray-900">Stock: {detailCards.stock}</p>
+      </div>
+        <div className="flex justify-center items-center flex-wrap">
+          <div className="flex justify-center items-center flex-col mb-10 mx-20">
+          <p className="text-2xl tracking-tight py-2 font-bold text-gray-900">Model: {detailCards.model}</p>
+          <p className="text-2xl tracking-tight py-2 text-gray-900">Price: ${detailCards.price}</p>
+          <p className="text-2xl tracking-tight py-2 text-gray-900">Stock: {detailCards.stock}</p>
           <div className="rating py-2">
             <input type="radio" name="rating-1" className="mask mask-star" checked />
             <input type="radio" name="rating-1" className="mask mask-star" />
@@ -74,23 +82,20 @@ export default function DetailCards() {
             <input type="radio" name="rating-1" className="mask mask-star" />
             <input type="radio" name="rating-1" className="mask mask-star" />
           </div> 
-          <div>
+          </div>
+          <div className="flex justify-center items-center flex-col mb-10 mx-20">
           <button
             type="submit"
-            className="mt-10 btn-details flex ml-24 items-center justify-center rounded-md  bg-indigo-600 py-3 px-8 text-base font-medium text-white  focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            className="mt-10 btn-details rounded-md  bg-indigo-600 py-3 px-8 text-base font-medium text-white  focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
           >Add favorites </button>
           <button
             onClick={() => dispatch(addToCart(detailCards))}
-            className="mt-10 btn-details flex ml-24 items-center justify-center rounded-md  bg-indigo-600 py-3 px-8 text-base font-medium text-white  focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            className="mt-10 btn-details rounded-md  bg-indigo-600 py-3 px-8 text-base font-medium text-white  focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
           >Add to cart</button>
-
+          <button className="btn mt-10" onClick={() => navigate(-1)}>Go back</button>
           </div>
         </div>
       </div>
-
-    </div>
-
-
 
   )
 }
