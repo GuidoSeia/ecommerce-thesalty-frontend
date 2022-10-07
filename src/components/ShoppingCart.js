9a03bb173133435e1f9b04db67c7cdf8820ebdec
ulisescolosimo <colosimo101@gmail.com>
2022-10-07 11:30:21 -0300

    Fix display flex center en administrador

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -16,7 +16,7 @@ const ShoppingCart = () => {
   let total = cart.reduce(addition, 0);
 
   return (
-<div tabIndex={0} className="mt-3 card flex justify-center items-center card-compact dropdown-content w-52 bg-black shadow" onClick={onclick} >
+<div tabIndex={0} className="mt-3 card flex justify-center items-center card-compact dropdown-content w-52 bg-base-100 shadow" onClick={onclick} >
 
 <div className="card-body" >
                         <span className="font-bold text-xl">{cart.length} items</span>
