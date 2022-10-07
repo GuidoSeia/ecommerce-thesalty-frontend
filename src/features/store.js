c05c3ea5055a49b63cc4c448602a6cc3c832721f
engelberthRetamal <engel.retamal.e@gmail.com>
2022-10-06 21:14:16 -0300

    Se agregó couponApi a store

--- src/features/store.js
+++ src/features/store.js
@@ -4,11 +4,13 @@ import usersAPI from './usersAPI'
 import loggedSlice from './loggedSlice'
 import cartSlice from './cartSlice'
 import codeSlice from './codeSlice'
+import couponApi from './couponApi'
 
 export default  configureStore({
     reducer: {
         [productsApi.reducerPath]: productsApi.reducer,
         [usersAPI.reducerPath]: usersAPI.reducer,
+        [couponApi.reducerPath]: couponApi.reducer,
         logged: loggedSlice,
         cart: cartSlice,
         code: codeSlice,
