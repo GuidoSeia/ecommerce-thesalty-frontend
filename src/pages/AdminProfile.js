d6b0b7e83db2adfbbf334ab790122797f91730e1
engelberthRetamal <engel.retamal.e@gmail.com>
2022-10-07 17:15:02 -0300

    Se creó endpoint para eliminar cupon y se agrego la funcionalidad en la pagina de admin

--- src/pages/AdminProfile.js
+++ src/pages/AdminProfile.js
@@ -1,14 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PageLayout from '../components/layout/PageLayout'
 import { Link as LinkRouter } from 'react-router-dom'
 import '../styles//AdminProfile.css'
+import { useDeleteCouponMutation } from '../features/couponApi'
+import { toast } from 'react-toastify'
 
 
-
-export default function AdminProfile({ functionCountdown }) {
+export default function AdminProfile({ functionCountdown, currentCouponId }) {
 
   let fnCountdown = functionCountdown
-  // ref={formRef}
+  let [deleteCoupon] = useDeleteCouponMutation()
+  const [coupon, setCoupon] = useState()
+
+  let handlDeleteCupon = (e) => {
+    e.preventDefault()
+    setCoupon("")
+
+    deleteCoupon(e.target.currentCode.value).then(
+      res => {
+        if (res.error) {
+          toast.error(res.error.data.message)
+        } else {
+          toast.success(res.data.message)
+        }
+      }
+    )
+  }
+
+  useEffect(() => {
+    setCoupon(currentCouponId)
+  }, [currentCouponId])
+  
+
+
+  // res.error
+
   return (
     <div>
       <PageLayout>
@@ -48,14 +74,35 @@ export default function AdminProfile({ functionCountdown }) {
               </label>
               <input type="text" name="discount" className="input input-bordered h-10" />
               <label className="label">
-                <span className="label-text text-white text-base text-center">select the number of days the coupon will be available</span>
+                <span className="label-text text-black text-base text-center">select the number of days the coupon will be available</span>
               </label>
               <input type="number" name="endTime" className="input input-bordered h-10" />
               <div className="form-control mt-6">
               </div>
-              <button className="btn btn-primary btn-home-page text-xs">Create!</button>
+              <div className='flex flex-col items-center'>
+                <button className="btn btn-primary btn-home-page text-xs" type='submit'>Create!</button>
+              </div>
             </form>
           </div>
+
+          <div className="cardContainer">
+            <figure className='w-full h-40 flex justify-center'>
+              <img width={150} src="https://cdn-icons-png.flaticon.com/512/2331/2331729.png" alt="Shoes" />
+            </figure>
+            <div className="subContainer">
+              <form onSubmit={handlDeleteCupon} className="h-full flex flex-col items-center justify-center">
+                <h2 className="">Remove the current coupon</h2>
+                <label className="label">
+                  <span className="label-text text-black text-base">Coupon code</span>
+                </label>
+                <input type="text" name="currentCode" className="input input-bordered h-10 text-xs w-full" value={coupon} disabled />
+                <div className="">
+                  <button className="btn btn-primary btn-home-page text-xs">Remove</button>
+                </div>
+              </form>
+            </div>
+          </div>
+
         </div>
 
         {/* 
