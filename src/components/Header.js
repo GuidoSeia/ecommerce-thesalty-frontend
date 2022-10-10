09ce1b35c6eb7fe1923c48443fc4d07d1f60b8fa
engelberthRetamal <engel.retamal.e@gmail.com>
2022-10-10 17:39:44 -0300

    Se ajustó z-index de menu del header

--- src/components/Header.js
+++ src/components/Header.js
@@ -69,7 +69,7 @@ export default function Header() {
           <label tabIndex={0} className="btn btn-ghost btn-circle">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
           </label>
-          <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+          <ul tabIndex={0} className="menu menu-compact dropdown-content z-50 mt-3 p-2 shadow bg-base-100 rounded-box w-52">
             <li><LinkRouter to="/home">Home</LinkRouter></li>
             <li><LinkRouter to="/products">Products</LinkRouter></li>
             <li><LinkRouter to="/aboutUs">About Us</LinkRouter></li>
