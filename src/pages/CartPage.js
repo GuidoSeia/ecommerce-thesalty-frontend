9204758c04345e07abe80c3d0e7bafc14a39cb84
your name <your github email address>
2022-10-12 19:05:44 -0300

    Fixes

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -175,7 +175,7 @@ export default function CartPage({ coupon }) {
                                     <div className="flex flex-col justify-center align-center gap-2">
                                         <p>$ {subTotalCart}</p>
                                         <div className="flex flex-wrap md:flex-nowrap justify-center items-center ">
-                                            {code == false ? <input type="text" placeholder="Enter code" ref={codeRef} className="input w-28" /> : <input type="text" placeholder="Enter code" ref={codeRef} disabled className="input w-28" />}
+                                            {code == false ? <input type="text" placeholder="Enter code" ref={codeRef} className="input w-32" /> : <input type="text" placeholder="Enter code" ref={codeRef} disabled className="input w-32" />}
 
                                             {code == false ? <button onClick={applyDiscount} className="h-1 ml-1 btn w-12 text-white">
                                                 Add
